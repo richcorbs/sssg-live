75b55fe868e65f4244b2392bca518f97e7abfd80
Rich Corbridge <rich.corbridge@gmail.com>
2025-01-03 19:07:20 -0700

    Handle domain better

--- server.ts
+++ server.ts
@@ -1,12 +1,10 @@
 import { getContext, RequestContext } from "./router.ts";
 import { handleDomainRequest, handleHostingRequest } from "./domainHandlers.ts";
 
-const ADMIN_DOMAIN = import.meta.url.includes("/Users/rich/Code") ? "http://domains.local:8000" : "https://api.sssg.dev";
-
 Deno.serve(async (req): Promise<Response> => {
   const ctx: RequestContext = await getContext(req);
 
-  if (ctx.HOSTNAME !== ADMIN_DOMAIN) {
+  if (ctx.HOSTNAME !== "0.0.0.0" && ctx.HOSTNAME !== "domains.local") {
     return await handleHostingRequest(ctx);
   } else {
     return await handleDomainRequest(ctx);
