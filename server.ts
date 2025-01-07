77575cfe58cff7898570d4cd4ce60cb621e4f535
Rich Corbridge <rich.corbridge@gmail.com>
2025-01-06 17:07:02 -0700

    Tweak for sub-domains

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { handleDomainRequest, handleHostingRequest } from "./domainHandlers.ts";
 Deno.serve(async (req): Promise<Response> => {
   const ctx: RequestContext = await getContext(req);
 
-  if (ctx.HOSTNAME !== "api.sssg.dev" && ctx.HOSTNAME !== "domains.local") {
+  if (ctx.HOSTNAME !== "0.0.0.0" && ctx.HOSTNAME !== "api.sssg.dev" && ctx.HOSTNAME !== "domains.local") {
     return await handleHostingRequest(ctx);
   } else {
     return await handleDomainRequest(ctx);
